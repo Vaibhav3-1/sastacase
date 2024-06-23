881b2cf187f1f5ecb50fd96510e56c5508f40210
Vaibhav3-1 <109387036+Vaibhav3-1@users.noreply.github.com>
2024-06-23 22:34:58 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
         <div className='h-full flex flex-col md:flex-row md:justify-between justify-center items-center'>
           <div className='text-center md:text-left pb-2 md:pb-0'>
             <p className='text-sm text-muted-foreground'>
-              &copy; {new Date().getFullYear()} All rights reserved
+              &copy; {new Date().getFullYear()} All rights reserved  
             </p>
           </div>
 
@@ -19,17 +19,7 @@ const Footer = () => {
               <Link
                 href='#'
                 className='text-sm text-muted-foreground hover:text-gray-600'>
-                Terms
-              </Link>
-              <Link
-                href='#'
-                className='text-sm text-muted-foreground hover:text-gray-600'>
-                Privacy Policy
-              </Link>
-              <Link
-                href='#'
-                className='text-sm text-muted-foreground hover:text-gray-600'>
-                Cookie Policy
+                @Vaibhav from SastaCase
               </Link>
             </div>
           </div>
